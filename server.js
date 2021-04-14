5e4f99a046f6ba289f00344e6cd545b46f45eeb6
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-04-14 15:08:35 -0400

    added html and js files and created heroku server

---
+++ server.js
@@ -0,0 +1,20 @@
+// Dependencies
+
+const express = require('express');
+const path = require('path');
+
+// Sets up the Express App
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Star Wars Characters (DATA)
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+// Starts the server to begin listening
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
