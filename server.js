e6a9a851f5d6dc85f4f3b12d06de19d5dee948cd
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-04-16 23:02:08 -0400

    added route to notes.html

--- server.js
+++ server.js
@@ -6,15 +6,15 @@ const path = require('path');
 // Sets up the Express App
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-// Star Wars Characters (DATA)
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
 // Starts the server to begin listening
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
