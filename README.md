18e0ace89962c1c5b27cc3ee20bcc0496178cf0f
himalayan <79290770+mattyb5000@users.noreply.github.com>
2021-04-16 23:36:12 -0400

    attempted other routes

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # note-taker
+## Description
+
+The Note Taker app is build using Express JS and will allow you to enter and save notes with a title.
+
+![Note Taker site image](/note-taker/public/assets/notetaker.png)
+
+## Deployed site
+You can find the deployed site here:
+
+[Note Taker site](https://tranquil-gorge-12598.herokuapp.com/)
--- server.js
+++ server.js
@@ -9,12 +9,22 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({
+    extended: true
+}));
 app.use(express.json());
 
-
+// Route takes you to home page
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+// Route takes you to notes page after clicking 'Get Started'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, './public/notes.html')));
+
+// Route connecting db.json
+app.get('/api/notes', (req, res) => res.json(__dirname, './db/db.json'));
+
+// Route to post new notes to notes page
+
 // Starts the server to begin listening
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
